aeb3c4a2bfa87a8d7ba5732038a3a2465f98410c
AaronGramajo <Aarongramajo85@gmail.com>
2023-12-13 00:54:10 -0300

    adapt to netlify server

---
+++ BackEnd/src/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const serverless = require('serverless-http');
+// const {Server: HttpServer} = require('http')
+
+const loggerCustom = require('../utils/log4js.js')
+const { initServer } = require('../app.js')
+require('dotenv').config()
+
+//////////////// Io server socket ////////////////
+const app = express()
+// const httpServer = new HttpServer(app)
+initServer(app)
+
+// const PORT = process.argv[2] || process.env.PORT || 8080
+// const server = httpServer.listen(PORT, () => {
+//     loggerCustom.info(`listening on port ${server.address().port}`)
+// }).on('error', (err)=> loggerCustom.error(`error en el servidor ${err}`))
+
+module.exports.handler = serverless(app)
